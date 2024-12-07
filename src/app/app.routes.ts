871b43192d1d2eca6c0768df3c9cc29e1d9b24a4
Rubenspv <rubenpv011992@gmail.com>
2024-12-07 19:53:42 +0100

    fix: default redirect to register route by now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,18 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadChildren: () =>
+      import('./auth.routes').then(m => m.authRoutes),
+  },
+  {
     path: 'auth',
     loadChildren: () =>
       import('./auth.routes').then(m => m.authRoutes),
-  }
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./auth.routes').then(m => m.authRoutes),
+  },
 ];
