92aa74f90ddf905b1da3e0577c00c791b1d1bebe
Rubenspv <rubenpv011992@gmail.com>
2025-03-30 19:46:57 +0200

    fix: env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://chatbot-microservice.vercel.app/api/chat',
-  dIdClientKey: 'cnViZW5wdjAxMTk5MkBnbWFpbC5jb20:Vh-E6dccGrxnXrPGQZysO',
+  dIdClientKey: 'Z29vZ2xlLW9hdXRoMnwxMTAyNTE5MzEwNDc0NjA1MDk0ODc6YnJ5N2t3VXZzRFhHSHhyd1hINk9S',
   dIdAgentId: 'agt_xxmFLzST'
 };
