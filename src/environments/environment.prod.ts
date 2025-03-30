7fe28beaedef3bbda6ae8124f5fecec1334aeadf
Rubenspv <rubenpv011992@gmail.com>
2025-03-30 19:16:53 +0200

    fix: udpate environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://chatbot-microservice.vercel.app/api/chat'
+  apiUrl: 'https://chatbot-microservice.vercel.app/api/chat',
+  dIdClientKey: 'cnViZW5wdjAxMTk5MkBnbWFpbC5jb20:Vh-E6dccGrxnXrPGQZysO',
+  dIdAgentId: 'agt_123'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/chat',
-  dIdClientKey: 'cnViZW5wdjAxMTk5MkBnbWFpbC5jb20:Vh-E6dccGrxnXrPGQZysO',
-  dIdAgentId: 'agt_123'
+  apiUrl: 'http://localhost:3000/api/chat'
 };
