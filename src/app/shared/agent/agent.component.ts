fd64fa5d14a7e618b87c24fab3e11835d867d07d
Rubenspv <rubenpv011992@gmail.com>
2025-03-30 19:15:34 +0200

    feat: added agent to test and change name page

---
+++ src/app/shared/agent/agent.component.ts
@@ -0,0 +1,28 @@
+import { Component, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { environment } from '../../../environments/environment';
+
+
+@Component({
+  selector: 'app-agent',
+  template: '',
+})
+export class AgentComponent {
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  ngOnInit() {
+    const script = this.renderer.createElement('script');
+    script.type = 'module';
+    script.src = 'https://agent.d-id.com/v1/index.js';
+    script.setAttribute('data-name', 'did-agent');
+    script.setAttribute('data-mode', 'fabio');
+    script.setAttribute('data-client-key', environment.dIdClientKey);
+    script.setAttribute('data-agent-id', environment.dIdAgentId);
+    script.setAttribute('data-monitor', 'true');
+
+    this.renderer.appendChild(this.document.body, script);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/chat'
+  apiUrl: 'http://localhost:3000/api/chat',
+  dIdClientKey: 'cnViZW5wdjAxMTk5MkBnbWFpbC5jb20:Vh-E6dccGrxnXrPGQZysO',
+  dIdAgentId: 'agt_123'
 };
