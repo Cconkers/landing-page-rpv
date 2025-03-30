4c2fb4ed9f49ce09254e6ab5462e0e318857f2dd
Rubenspv <rubenpv011992@gmail.com>
2025-03-30 19:23:20 +0200

    fix: environment data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://chatbot-microservice.vercel.app/api/chat',
   dIdClientKey: 'cnViZW5wdjAxMTk5MkBnbWFpbC5jb20:Vh-E6dccGrxnXrPGQZysO',
-  dIdAgentId: 'agt_123'
+  dIdAgentId: 'agt_xxmFLzST'
 };
