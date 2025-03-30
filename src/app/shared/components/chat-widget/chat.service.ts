f8c0c6f3a004bc34c13cb40ef7a9d75d6ee35ccb
Rubenspv <rubenpv011992@gmail.com>
2025-03-30 04:41:20 +0200

    feat: added environment routing

--- src/app/shared/components/chat-widget/chat.service.ts
+++ src/app/shared/components/chat-widget/chat.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environment';
 
 @Injectable({ providedIn: 'root' })
 export class ChatService {
-  private apiUrl = 'http://localhost:3000/api/chat'; // Cambia por el dominio de producción si aplica
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
